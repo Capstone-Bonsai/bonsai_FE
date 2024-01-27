b34e82875d9fc1d3a195f96503ff41b4bc654089
dangphan15 <minhdang12345c@gmail.com>
2024-01-27 11:51:15 +0700

    fix home page

---
+++ src/data/AllCategory.js
@@ -0,0 +1,33 @@
+import category1 from "../assets/cat1.png";
+import category2 from "../assets/cat2.png";
+import category3 from "../assets/cat3.png";
+import category4 from "../assets/cat4.png";
+
+const categoryList = [
+    {
+      id: 1,
+      name: "Tiểu cảnh gốm vẽ 26",
+      amount: 1,
+      image: category1,
+    },
+    {
+      id: 2,
+      name: "Tiểu cảnh gốm vẽ 26",
+      amount: 1,
+      image: category2,
+    },
+    {
+      id: 3,
+      name: "Tiểu cảnh gốm vẽ 26",
+      amount: 1,
+      image: category3,
+    },
+    {
+      id: 4,
+      name: "Tiểu cảnh gốm vẽ 26",
+      amount: 1,
+      image: category4,
+    },
+  ];
+
+  export { categoryList };
--- src/pages/HomePage/SellProducts.jsx
+++ src/pages/HomePage/SellProducts.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setTopProducts, setBonsaiOffice } from "../../redux/productSlice";
 import { topProducts, bonsaiOffice } from "../../data/TopProducts";
+import { categoryList } from "../../data/AllCategory";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 function SellProducts() {
   const dispatch = useDispatch();
   const { topProductDTO } = useSelector((state) => state.product);
@@ -33,7 +36,7 @@ function SellProducts() {
           </a>
         ))}
       </div>
-      <div>
+      <div className="mb-6">
         <div className="flex justify-between mt-5 border-b-2 pb-1">
           <div className="text-[#00B214] font-bold text-2xl">
             CÂY CẢNH VĂN PHÒNG
@@ -45,20 +48,33 @@ function SellProducts() {
         <div className="flex flex-wrap justify-between">
           {bonsaiOfficeDTO.map((office) => (
             <div
-              className="mt-5 w-[270px] drop-shadow-lg bg-[#ffffff] h-[355px] "
+              className="mt-5 w-[270px] drop-shadow-lg bg-[#ffffff] h-[370px] "
               key={office.id}
             >
               <img src={office.image} alt="" />
-              <div className="text-center my-5">
-                <div className="text-[#1E7100]">{office.name}</div>
-                <div className="text-[#E04C78]">{office.price}</div>
+              <div className="pt-3 px-3">
+                <div className="mb-3">
+                  <Link className="text-[#1E7100] text-lg hover:text-[#333333]">{office.name}</Link>
+                </div>
+                <div className="grid grid-cols-3">
+                  <div class="flex items-center">
+                    <div className="text-[#E04C78] text-xl font-semibold">{office.price}</div>
+                  </div>
+                  <div class="col-end-4 flex justify-end">
+                    <Link
+                      className="text-[20px] w-[40px] h-[40px] bg-[#f2f2f2] flex items-center border pl-2 rounded-[100%] 
+          border-[#ffffff]-500 border-opacity-50 border-opacity-50 hover:bg-[#3A994A] hover:text-[#ffffff]"
+                    >
+                      <ShoppingCartOutlined />
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
         </div>
-        
       </div>
-      <div>
+      <div className="mb-6">
         <div className="flex justify-between mt-5 border-b-2 pb-1">
           <div className="text-[#00B214] font-bold text-2xl">
             CÂY CẢNH VĂN PHÒNG
@@ -70,18 +86,55 @@ function SellProducts() {
         <div className="flex flex-wrap justify-between">
           {bonsaiOfficeDTO.map((office) => (
             <div
-              className="mt-5 w-[270px] drop-shadow-lg bg-[#ffffff] h-[355px] "
+              className="mt-5 w-[270px] drop-shadow-lg bg-[#ffffff] h-[370px] "
               key={office.id}
             >
               <img src={office.image} alt="" />
-              <div className="text-center my-5">
-                <div className="text-[#1E7100]">{office.name}</div>
-                <div className="text-[#E04C78]">{office.price}</div>
+              <div className="pt-3 px-3">
+                <div className="mb-3">
+                  <Link className="text-[#1E7100] text-lg hover:text-[#333333]">{office.name}</Link>
+                </div>
+                <div className="grid grid-cols-3">
+                  <div class="flex items-center">
+                    <div className="text-[#E04C78] text-xl font-semibold">{office.price}</div>
+                  </div>
+                  <div class="col-end-4 flex justify-end">
+                    <Link
+                      className="text-[20px] w-[40px] h-[40px] bg-[#f2f2f2] flex items-center border pl-2 rounded-[100%] 
+          border-[#ffffff]-500 border-opacity-50 border-opacity-50 hover:bg-[#3A994A] hover:text-[#ffffff]"
+                    >
+                      <ShoppingCartOutlined />
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
         </div>
-        
+      </div>
+
+      <div className="mb-6">
+        <div className="flex justify-between mt-5 border-b-2 pb-1">
+          <div className="text-[#00B214] font-bold text-2xl">
+            DANH MỤC NỔI BẬT
+          </div>
+        </div>
+        <div className="flex flex-wrap justify-between">
+          {categoryList.map((office) => (
+            <Link
+              className="mt-5 w-[270px] drop-shadow-lg bg-[#ffffff] h-[355px] bg-local bg-no-repeat bg-cover hover:opacity-50"
+              style={{ backgroundImage: `url(${office.image})` }}
+              key={office.id}
+            >
+              <div className="text-center flex flex-col-reverse h-[100%] py-4">
+                <div className="font-normal">{office.amount} sản phẩm</div>
+                <div className="text-lg font-semibold uppercase">
+                  {office.name}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
