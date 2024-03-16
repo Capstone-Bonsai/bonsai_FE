2b37ce1f3aca2ef68b90274e82d71ad9d477da50
dangphan15 <minhdang12345c@gmail.com>
2024-03-16 21:36:05 +0700

    Update Bonsai.jsx

--- src/pages/Bonsai/Bonsai.jsx
+++ src/pages/Bonsai/Bonsai.jsx
@@ -32,16 +32,12 @@ function Bonsai() {
   );
   const loading = useSelector((state) => state.bonsai.loading);
   const keyword = location.state?.keyword;
-  const categories = useSelector(
-    (state) => state.category.allCategoryDTO?.items
-  );
-  const styles = useSelector((state) => state.style.styleDTO.items);
 
   useEffect(() => {
     dispatch(allCategory());
     dispatch(allStyle());
   }, []);
-  const dispatch = useDispatch();
+
   useEffect(() => {
     const payload = {
       pageIndex: pageIndex - 1,
@@ -54,7 +50,15 @@ function Bonsai() {
     };
 
     dispatch(fetchAllBonsai(payload));
-  }, [dispatch, pageIndex, pageSize, priceRange, selectedCategories, selectStyle, keyword]);
+  }, [
+    dispatch,
+    pageIndex,
+    pageSize,
+    priceRange,
+    selectedCategories,
+    selectStyle,
+    keyword,
+  ]);
 
   const handleResetFilter = () => {
     setSelectedCategories();
@@ -85,7 +89,7 @@ function Bonsai() {
                   Phân loại
                 </div>
                 <button onClick={handleResetFilter}>
-                  Tắt bộ lọc <RedoOutlined className="pl-2"/>
+                  Tắt bộ lọc <RedoOutlined className="pl-2" />
                 </button>
               </div>
               {categories?.map((category) => (
