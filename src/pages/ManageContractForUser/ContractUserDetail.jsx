a400b0d059e6f9cdc3aa16676a917381794697da
thasine2k1 <thaihoang03122001@gmail.com>
2024-04-12 17:27:35 +0700

    post complaint

--- src/pages/ManageContractForUser/ContractUserDetail.jsx
+++ src/pages/ManageContractForUser/ContractUserDetail.jsx
@@ -16,9 +16,9 @@ function ContractUserDetail(props) {
   const dispatch = useDispatch();
   const contractId = props.contractId;
   const location = useLocation();
+  const [apiContractLoading, setApiContractLoading] = useState(false);
   const [loading, setLoading] = useState(false);
   console.log(loading);
-
   useEffect(() => {
     setLoading(true);
     dispatch(contractDetailById(contractId))
@@ -27,7 +27,7 @@ function ContractUserDetail(props) {
         console.error("Error fetching contract detail:", error);
         setLoading(false);
       });
-  }, [contractId]);
+  }, [contractId, apiContractLoading]);
   const { contractDetail } = useSelector((state) => state.contract);
 
   const handlePaymentContract = async (contractId) => {
@@ -40,119 +40,156 @@ function ContractUserDetail(props) {
   };
   return (
     <>
-      {/* {loading ? (
+      {loading ? (
         <Loading loading={loading} isRelative={true} />
-      ) : ( */}
-      <div className="w-full relative">
-        <button
-          className="absolute top-[-10px] left-[-10px]"
-          onClick={() => props.setSelectedDetail(false)}
-        >
-          <LeftCircleOutlined className="text-[20px]" />
-        </button>
-        <div className="mt-3 h-full">
-          <div className="text-center text-lg font-bold">Hợp đồng</div>
-          <div className="text-center underline">{contractDetail.id}</div>
-          <div className="font-bold">1. Thông tin hợp đồng: </div>
-          <div className="border p-3">
-            <div className="flex gap-2 border-b">
-              Thời gian làm việc:
-              <div>
-                {new Date(contractDetail.startDate).toLocaleDateString()}
-              </div>
-              -
-              <div>{new Date(contractDetail.endDate).toLocaleDateString()}</div>
-            </div>
-            <div>Khoảng cách: {contractDetail?.distance}m</div>
-            <div>
-              Diện tích vườn: {contractDetail.gardenSquare}m<sup>2</sup>
-            </div>
-            <div className="flex justify-end">
-              <div className="border w-[30%] p-2  rounded-[10px]">
-                <div className="border-b">
-                  <div>
-                    Chi phí dịch vụ: {formatPrice(contractDetail.standardPrice)}
-                  </div>
-                  <div>
-                    Phụ phí: {formatPrice(contractDetail.surchargePrice)}
-                  </div>
-                </div>
+      ) : (
+        <div className="w-full relative">
+          <button
+            className="absolute top-[-10px] left-[-10px]"
+            onClick={() => props.setSelectedDetail(false)}
+          >
+            <LeftCircleOutlined className="text-[20px]" />
+          </button>
+          <div className="mt-3 h-full">
+            <div className="text-center text-lg font-bold">Hợp đồng</div>
+            <div className="text-center underline">{contractDetail.id}</div>
+            <div className="font-bold">1. Thông tin hợp đồng: </div>
+            <div className="border p-3">
+              <div className="flex gap-2 border-b">
+                Thời gian làm việc:
                 <div>
-                  Tổng:{" "}
-                  <span className="text-[#3a9943]">
-                    {" "}
-                    {formatPrice(contractDetail.totalPrice)}
-                  </span>
+                  {new Date(contractDetail.startDate).toLocaleDateString()}
+                </div>
+                -
+                <div>
+                  {new Date(contractDetail.endDate).toLocaleDateString()}
+                </div>
+              </div>
+              <div>Khoảng cách: {contractDetail?.distance}m</div>
+              <div>
+                Diện tích vườn: {contractDetail.gardenSquare}m<sup>2</sup>
+              </div>
+              <div className="flex justify-end">
+                <div className="border w-[30%] p-2  rounded-[10px]">
+                  <div className="border-b">
+                    <div>
+                      Chi phí dịch vụ:{" "}
+                      {formatPrice(contractDetail.standardPrice)}
+                    </div>
+                    <div>
+                      Phụ phí: {formatPrice(contractDetail.surchargePrice)}
+                    </div>
+                  </div>
+                  <div>
+                    Tổng:{" "}
+                    <span className="text-[#3a9943]">
+                      {" "}
+                      {formatPrice(contractDetail.totalPrice)}
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="flex justify-end mt-2">
+            <div className="flex justify-end mt-2">
+              {contractDetail.contractStatus == 1 ||
+              contractDetail.contractStatus == 4 ? (
+                <button
+                  onClick={() => handlePaymentContract(contractDetail.id)}
+                  className="btn hover:bg-[#3a9943] hover:text-[#ffffff]"
+                >
+                  Tiến hành thanh toán
+                </button>
+              ) : (
+                ""
+              )}
+            </div>
             {contractDetail.contractStatus == 1 ||
-            contractDetail.contractStatus == 4 ? (
-              <button
-                onClick={() => handlePaymentContract(contractDetail.id)}
-                className="btn hover:bg-[#3a9943] hover:text-[#ffffff]"
-              >
-                Tiến hành thanh toán
-              </button>
-            ) : (
+            contractDetail.contractStatus == 4 ||
+            contractDetail.contractStatus == 5 ? (
               ""
-            )}
-          </div>
-          {contractDetail.contractStatus == 1 ||
-          contractDetail.contractStatus == 4 ||
-          contractDetail.contractStatus == 5 ? (
-            ""
-          ) : (
-            <>
-              <div className="font-bold">Tiến độ công việc: </div>
-              <div className="overflow-x-auto">
-                <table className="w-full table border">
-                  {/* <colgroup className="border">
+            ) : (
+              <>
+                <div className="font-bold">Tiến độ công việc: </div>
+                <div className="overflow-x-auto">
+                  <table className="w-full table border">
+                    {/* <colgroup className="border">
                       <col className="w-[20%]" />
                       <col className="w-[40%]" />
                       <col className="w-[40%]" />
                     </colgroup> */}
+                    <thead>
+                      <tr className="font-bold text-lg">
+                        <th className="">STT</th>
+                        <th>Tên công việc</th>
+                        <th>Thời gian hoàn thành</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {contractDetail?.taskOfContracts?.map((task, index) => (
+                        <tr key={task.id} className="">
+                          <td>{index + 1}</td>
+                          <td>{task.name}</td>
+                          <td>
+                            {task?.completedTime != null
+                              ? new Date(
+                                  task?.completedTime
+                                ).toLocaleDateString()
+                              : "Chưa hoàn thành"}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <div className="flex justify-between mt-3 items-center">
+                  <div className="font-bold">Khiếu nại: </div>
+                  <button
+                    onClick={() =>
+                      document.getElementById("modal_complain").showModal()
+                    }
+                    className="btn outline-none font-normal hover:text-[#fff] hover:bg-[#3a9943] mr-[100px]"
+                  >
+                    Thêm khiếu nại
+                  </button>
+                  <ModalComplain
+                    contractDetailById={contractDetailById}
+                    contractId={contractDetail.id}
+                    setApiContractLoading={setApiContractLoading}
+                  />
+                </div>
+                <table className="w-full table">
                   <thead>
-                    <tr className="font-bold text-lg">
-                      <th className="">STT</th>
-                      <th>Tên công việc</th>
-                      <th>Thời gian hoàn thành</th>
+                    <tr>
+                      <th>STT</th>
+                      <th>Chi tiết khiếu nại</th>
+                      <th>Trạng Thái</th>
+                      <th>Lý do bị từ chối</th>
+                      <th>Hình ảnh</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {contractDetail?.taskOfContracts?.map((task, index) => (
-                      <tr key={task.id} className="">
-                        <td>{index + 1}</td>
-                        <td>{task.name}</td>
-                        <td>
-                          {task?.completedTime != null
-                            ? task?.completedTime
-                            : "Chưa hoàn thành"}
-                        </td>
+                    {contractDetail?.complaints?.length > 0 ? (
+                      contractDetail?.complaints.map((complaint, index) => (
+                        <tr key={complaint.id}>
+                          <td>{index + 1}</td>
+                          <td>{complaint.detail}</td>
+                          <td>{complaint.complaintStatus}</td>
+                          <td>{complaint.cancelReason}</td>
+                          <td>{complaint.complaintImages}</td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td></td>
                       </tr>
-                    ))}
+                    )}
                   </tbody>
                 </table>
-              </div>
-              <div className="flex justify-between mt-3 items-center">
-                <div className="font-bold">Khiếu nại: </div>
-                <button
-                  onClick={() =>
-                    document.getElementById("modal_complain").showModal()
-                  }
-                  className="btn font-normal hover:text-[#fff] hover:bg-[#3a9943] mr-[100px]"
-                >
-                  Thêm khiếu nại
-                </button>
-                <ModalComplain />
-              </div>
-            </>
-          )}
+              </>
+            )}
+          </div>
         </div>
-      </div>
-      {/* )} */}
+      )}
     </>
   );
 }
